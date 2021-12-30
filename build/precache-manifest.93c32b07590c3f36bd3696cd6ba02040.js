75e8dbb71d133820f3b742a9708d9c7c9c76368c
fabiomonteiroeu <email@dominio.com>
2021-12-30 17:50:06 -0300

    my first commit

---
+++ build/precache-manifest.93c32b07590c3f36bd3696cd6ba02040.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19d4a5ae6253ba7b5a1b4ab10f141e0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "855396643db388b3b4d9",
+    "url": "/static/css/main.d22cedb7.chunk.css"
+  },
+  {
+    "revision": "1ef1b47af3824133994d",
+    "url": "/static/js/2.160a70da.chunk.js"
+  },
+  {
+    "revision": "28b5e57e1e89ae408eee8d8f4d692c7c",
+    "url": "/static/js/2.160a70da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "855396643db388b3b4d9",
+    "url": "/static/js/main.efcb8277.chunk.js"
+  },
+  {
+    "revision": "22563380209b4ba4252f",
+    "url": "/static/js/runtime-main.1f46d0df.js"
+  }
+]);
